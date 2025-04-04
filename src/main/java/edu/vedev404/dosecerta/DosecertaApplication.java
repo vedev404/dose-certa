ef419c64b13486cbc0dfdb8b5f2cb1c342906c3b
almeida.veronica <vedev404@gmail.com>
2025-04-04 07:22:26 -0300

    primeiro commit

---
+++ src/main/java/edu/vedev404/dosecerta/DosecertaApplication.java
@@ -0,0 +1,13 @@
+package edu.vedev404.dosecerta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DosecertaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DosecertaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/vedev404/dosecerta/DosecertaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.vedev404.dosecerta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DosecertaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
